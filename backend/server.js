deccb22b869ebb0c366b63dc5b168ace8f3f197e
Rahul Rajput <rahulrajput808160@gmail.com>
2025-10-28 06:55:25 +0530

    removed extra

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ import analyticsRoutes from './routes/analytics.route.js';
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
-app.use(express.static());
+
 
 
 
